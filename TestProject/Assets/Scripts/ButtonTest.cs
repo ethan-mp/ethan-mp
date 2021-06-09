693fc875e440b8157ef36fc11c45b38f2f5d6b20
박민우 <mp15@MPs-MBPro-15.local>
2021-06-09 11:20:21 +0900

    First Commit

---
+++ TestProject/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Button testButton;
+    public AudioSource audioSource;
+    public AudioClip engineSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonPressed()
+    {
+        Debug.Log("Button Pressed!!!");
+        testButton.GetComponentInChildren<Text>().text = "YES";
+    }
+}
